7fb9abc1a61574c0e8fc24cf42db11ec71dfc380
Samet Sevindi <samet.sevindi@gmail.com>
2020-08-30 16:41:35 +0300

    fixed the downward movement of paddles

--- main.js
+++ main.js
@@ -14,18 +14,18 @@ let height = window.innerHeight;
 let px = "px";
 
 let paddleHeight = 200;
-let paddleWidth = 25;
+let paddleWidth = 15;
 let paddleSpeed = 5;
 
 // function to delete px for calculations
-
 function rmv(size) {
-  return size.replace("px", "");
+  return parseInt(size.replace("px", ""));
 }
 
 // track key strokes and record in an array in order to use more than one key at a time
 let key = [];
 onkeydown = onkeyup = function (e) {
+  e = e || event;
   key[e.keyCode] = e.type == "keydown";
 };
 
@@ -35,6 +35,7 @@ rightPaddle.style.top = height / 2 - 100 + px;
 
 // move paddle divs according to keystrokes
 function keyPressed() {
+  // moves paddle up until border when w key pressed
   if (key[87]) {
     if (rmv(leftPaddle.style.top) - paddleSpeed <= 0) {
       leftPaddle.style.top = 0 + px;
@@ -43,14 +44,16 @@ function keyPressed() {
     }
   }
 
+  // moves paddle down until border when s key pressed
   if (key[83]) {
-    if (rmv(leftPaddle.style.top) > height - paddleHeight) {
-      leftPaddle.style.top = height - paddleHeight;
+    if (rmv(leftPaddle.style.top) + paddleSpeed > height - paddleHeight) {
+      leftPaddle.style.top = height - paddleHeight + px;
     } else {
       leftPaddle.style.top = rmv(leftPaddle.style.top) + paddleSpeed + px;
     }
   }
 
+  // moves paddle down until border when arrowup key pressed
   if (key[38]) {
     if (rmv(rightPaddle.style.top) - paddleSpeed <= 0) {
       rightPaddle.style.top = 0 + px;
@@ -59,14 +62,16 @@ function keyPressed() {
     }
   }
 
+  // moves paddle down until border when arrowdown key pressed
   if (key[40]) {
-    if (rmv(rightPaddle.style.top) > height - paddleHeight) {
-      rightPaddle.style.top = height - paddleHeight;
+    if (rmv(rightPaddle.style.top) + paddleSpeed > height - paddleHeight) {
+      rightPaddle.style.top = height - paddleHeight + px;
     } else {
       rightPaddle.style.top = rmv(rightPaddle.style.top) + paddleSpeed + px;
     }
   }
 }
+
 
 setInterval(function () {
   keyPressed();
